b11b4686b72a4ac4c472cd84f47105a0354ede8f
Dansday <akbaryudhanto49@gmail.com>
2026-04-02 16:26:35 +0800

    Fixing owner crash at register

--- src/routes/(auth)/register/+page.server.ts
+++ src/routes/(auth)/register/+page.server.ts
@@ -4,7 +4,11 @@ import type { PageServerLoad } from './$types';
 export const load: PageServerLoad = ({ locals, url }) => {
 	if (locals.user.authenticated) redirect(302, '/');
 
-	const canRegister = !locals.user.authenticated && (('can_register' in locals.user && locals.user.can_register) || Boolean(token));
+	const token = url.searchParams.get('token') ?? null;
+
+	const canRegister =
+		!locals.user.authenticated &&
+		(('can_register' in locals.user && locals.user.can_register) || Boolean(token));
 
 	if (!canRegister) redirect(302, '/login');
 

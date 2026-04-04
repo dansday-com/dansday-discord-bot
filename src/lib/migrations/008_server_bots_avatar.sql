dfb287f5fa7f9d1734ae2d7a4dbcacf9fa66f45c
Dansday <akbaryudhanto49@gmail.com>
2026-04-05 05:11:26 +0800

    1047: Cleanup

--- src/lib/migrations/008_server_bots_avatar.sql
+++
@@ -1,5 +0,0 @@
-SET @col = (SELECT COUNT(*) FROM information_schema.columns WHERE table_schema = DATABASE() AND table_name = 'server_bots' AND column_name = 'bot_icon');
-SET @s = IF(@col = 0, 'ALTER TABLE server_bots ADD COLUMN bot_icon TEXT NULL', 'SELECT 1');
-PREPARE st FROM @s;
-EXECUTE st;
-DEALLOCATE PREPARE st;
